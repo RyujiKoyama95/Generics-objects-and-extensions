be895caadf91f2319ac0ce00660a42f894748fd8
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-06-19 23:24:30 +0900

    Add generics

--- app/src/main/java/com/example/kotlingenerics/Playground.kt
+++ app/src/main/java/com/example/kotlingenerics/Playground.kt
@@ -4,6 +4,37 @@ package com.example.kotlingenerics
  * 1.Make a reusable class with generics
  */
 
+// クイズを各クラスで表現
+class FillInTheBlankQuestion(
+    val questionText: String,
+    val answer: String,
+    val difficulty: String
+)
+class TrueOrFalseQuestion(
+    val questionText: String,
+    val answer: Boolean,
+    val difficulty: String
+)
+class NumericQuestion(
+    val questionText: String,
+    val answer: Int,
+    val difficulty: String
+)
+
+// genericsで表現
+class Question<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: String
+)
+
+// genericsで表現したクラスをインスタンス化
+fun main() {
+    val que1 = Question("aaa", "bbb", "easy")
+    val que2 = Question("aaa", 1, "easy")
+    val que3 = Question("aaa", true, "easy")
+}
+
 /**
  * 2.Use an enum class
  */
