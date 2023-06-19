307a559bdf95b9a51a0c8bd474476d0cebc51ba3
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-06-19 23:12:11 +0900

    Setup playground

--- app/src/main/java/com/example/kotlingenerics/Playground.kt
+++ app/src/main/java/com/example/kotlingenerics/Playground.kt
@@ -1,2 +1,29 @@
 package com.example.kotlingenerics
 
+/**
+ * 1.Make a reusable class with generics
+ */
+
+/**
+ * 2.Use an enum class
+ */
+
+/**
+ * 3.Use a data class
+ */
+
+/**
+ * 4.Use a singleton object
+ */
+
+/**
+ * 5.Extend classes with new properties and methods
+ */
+
+/**
+ * 6.Rewrite extension functions using interfaces
+ */
+
+/**
+ * 7.Use scope functions to access class properties and methods
+ */
